ccdb6aee4a0549f9db27cc0e7eeabde16d1eb0b5
unknown <anjumarnab050@gmail.com>
2025-05-18 22:13:39 +0600

    I:

--- lib/service/app_script_url.dart
+++ lib/service/app_script_url.dart
@@ -1,2 +1,1 @@
-const String APP_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbxc6CTjzqP9wjsj32prRBwQ73fIhPKq0lv1vmTjtkflD23Gu_Oi1OXS3ZXhJ7NCqt3P/exec";
+const String APP_SCRIPT_URL = "";
